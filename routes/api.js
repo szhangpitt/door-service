1d0ed2dd640a9ccd3dc1d00832110722e50e69ca
Shaopeng Zhang <szhang351@bloomberg.net>
2015-12-28 13:48:30 -0500

    json sample data

--- routes/api.js
+++ routes/api.js
@@ -1,106 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-
-// router.get('/user', function (req, res, next) {
-//     res.json({
-//         'result': {
-//             id: 1
-//         }
-//     });
-// });
-
 router.get('/constants', function (req, res, next) {
-    res.json({
-    'LOT_SIZE_UNIT': [{
-            value: 'sqft',
-            label: 'sqft'
-        }, {
-            value: 'acre',
-            label: 'acre'
-        }],
-    'PROPERTY_TYPES': [{
-            label: 'Condo',
-            value: 'condo',
-        }, {
-            label: 'Single',
-            value: 'single'
-        }, {
-            label: 'Semi',
-            value: 'semi'
-        }, {
-            label: 'Town',
-            value: 'town'
-        }, {
-            label: 'Co-op',
-            value: 'co-op'
-        }, {
-            label: 'other',
-            value: 'other'
-        }],
-    'PROPERTY_STATUS': [{
-              value: 'open',
-              label: 'Open'
-          }, {
-              value: 'initial',
-              label: 'Initial'
-          }]
-    });
+    res.json(require('./json-constants'));
 });
 
 
-// router.post('/login', function (req, res, next) {
-//     res.json({
-//       "count": 1,
-//       "start_element": 0,
-//       "num_elements": 0,
-//       "debug_info": {
-//         "start_time": 1447937761078,
-//         "duration": 118
-//       }
-//     });
-// });
+router.post('/login', function (req, res, next) {
+    res.json(require('./json-login'));
+});
 
 
 router.use('/user', function (req, res, next) {
-    res.json({
-      "result": {
-        "id": 1,
-        "email": "foo@bar.com",
-        "type": "buyer",
-        "interested_area": [
-          {
-            "area": "shanghai"
-          }
-        ],
-        "interested_property_type": [
-          {
-            "property_type": "condo"
-          },
-          {
-            "property_type": "sfh"
-          }
-        ],
-        "interested_factor": [
-          {
-            "factor": "school"
-          }
-        ],
-        "interest_time_to_buy": "month-12"
-      },
-      "count": 1,
-      "debug_info": {
-        "start_time": 1450254794088,
-        "duration": 14
-      }
-    });
+    res.json(require('./json-user'));
 });
 
-// router.post('/property', function (req, res, next) {
-    
-// })
-// 
-// 
+
+router.get('/geo', function (req, res) {
+    res.json(require('./json-geo'));
+});
+
+router.get('/state', function (req, res) {
+    res.json(require('./json-state'));
+});
 
 /* GET users listing. */
 router.use('/', function(req, res, next) {

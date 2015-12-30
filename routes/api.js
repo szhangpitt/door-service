b8cfc1eaa0b52619c42e7d0c422a11ad4ad7b60d
Shaopeng Zhang <szhang351@bloomberg.net>
2015-12-30 15:53:12 -0500

    service

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,7 @@ function tok (req, res, next) {
         req.decoded = decoded;
         console.log('decoded', decoded);
         next();
-    })
+    });
 }
 
 function delay (req, res, next) {
@@ -51,49 +51,33 @@ router.post('/auth', delay, function (req, res, next) {
     var user;
     var token;
 
-    if (req.body.password !== 'password') {
+    if (req.body.password !== 'buyer' && req.body.password !== 'agent') {
         return res.status(401)
            .json(require('./json-login-fail'));
     }
 
-    user = require('./json-user').result;
+    user = lodash.assign({
+        ui: 1,
+        ut: req.body.password,
+        action: {
+            'profile': true
+        }
+    }, require('./json-user').result);
 
     token = jwt.sign(user, SECRET, {expiresIn: '20m'});
 
     res.header('Authorization', token);
     res.json(
         lodash.assign({
-            'token': token
+            'jwt': token,
+            'flash_message': 'Welcome!'
         }, require('./json-login'))
     );
 });
 
-router.post('/logout', delay, function (req, res, next) {
-    res.status(200).send();
-});
-
-router.post('/user', delay, function (req, res, next) {
-    res.json(require('./json-user'));
-});
-
-router.post('/validate-email', delay, tok, function (req, res, next) {
-    if (req.body.code === 'asdf') {
-        res.json(require('./json-user'));
-    } else {
-        res.status(401).json(err('validate email fail'));
-    }
-});
 
 router.get('/constants', function (req, res, next) {
     res.json(require('./json-constants'));
-});
-
-router.put('/user', delay, tok, function (req, res, next) {
-    res.json(require('./json-user'));
-});
-
-router.get('/user', tok, function (req, res, next) {
-    res.json(require('./json-user'));
 });
 
 router.get('/geo', function (req, res) {
@@ -102,6 +86,58 @@ router.get('/geo', function (req, res) {
 
 router.get('/state', function (req, res) {
     res.json(require('./json-state'));
+});
+
+router.post('/user', delay, function (req, res, next) {
+    res.json(require('./json-user'));
+});
+
+router.put('/user/:id', delay, tok, function (req, res, next) {
+    res.json(require('./json-user'));
+});
+
+router.get('/user/:id', tok, function (req, res, next) {
+    res.json(require('./json-user'));
+});
+
+router.post('/user/:id/validate-email', delay, tok, function (req, res, next) {
+    if (req.body.code === 'asdf') {
+        res.json(lodash.assign({
+            message: 'validated!'
+        }, require('./json-user')));
+    } else {
+        res.status(400).json(err('validate email fail'));
+    }
+});
+
+router.post('/user/:id/re-validate-email', delay, tok, function (req, res, next) {
+        res.json(require('./json-user'));
+});
+
+router.get('/user/:id/actions', tok, function (req, res, next) {
+    // for action, see https://design.google.com/icons/
+    res.json({
+        'results': [
+            {
+                'action': 'profile',
+                'text': 'Update your profile to get most accurate results.',
+            }, {
+                'action': 'validate',
+                'text': 'Validate your email address.',
+            }, {
+                'action': 'property',
+                'text': 'There are 2 new properties on the market.',
+            }, {
+                'action': 'trending_up',
+                'text': '731 Lexington has an increased price!',
+                'path': '/property/1',
+            }, {
+                'action': 'trending_down',
+                'text': '731 Lexington has a markdown price!',
+                'path': '/property/1',
+            }
+        ]
+    });
 });
 
 router.get('/property/:id', function (req, res) {
